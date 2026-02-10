0dbca622bea70c9dd798cfe4baa1547e8a024c45
DanzillaDanny <thenumberonesonsband@gmail.com>
2026-02-09 19:31:41 -0600

    updated classes

---
+++ src/main/java/com/example/demo/model/Teachers.java
@@ -0,0 +1,46 @@
+package com.example.demo.model.Teachers;
+
+import jakarta.persistence.Entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
+public class Teachers {
+   @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private String name;
+    private String email;
+
+    public Teachers() {
+    }
+
+    public Teachers(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
